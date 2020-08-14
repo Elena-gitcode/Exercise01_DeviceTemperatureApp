7f00ec7df0e5290cccaba453a88527a48a283c4b
Elena Ivanova <Elena.Ivanova.acc@gmail.com>
2020-08-14 23:06:35 +1000

    SomeCosmeticChanges

--- Program.cs
+++ Program.cs
@@ -26,36 +26,47 @@ namespace Exercise01
 
             //Previous temperature reading   
 
-            const string OPERATIONAL_FILE_NAME = "C:\\Temp\\TemperatureReading.txt";
+            const string OPERATIONAL_FILE_NAME = "TemperatureReading.txt";
             int previouslyRecordedTemp = inputCurrentTemp;
 
             if (File.Exists(OPERATIONAL_FILE_NAME))
             {
                 previouslyRecordedTemp = Convert.ToInt32(File.ReadAllText(OPERATIONAL_FILE_NAME));
-            }                          
+            } 
+            
             File.WriteAllText(OPERATIONAL_FILE_NAME, inputCurrentTemp.ToString());
 
 
             //Temperature analysis
 
             if (inputCurrentTemp < 30)
-            Console.WriteLine("WARNING! The current temperature is below the threshold of 30C!");
+            {
+                Console.WriteLine("WARNING! The current temperature is below the threshold of 30C!");
+            }
             else
             if (inputCurrentTemp > 90)
-            Console.WriteLine("WARNING! The current temperature is above the threshold of 90C!");
+            { 
+                Console.WriteLine("WARNING! The current temperature is above the threshold of 90C!");
+            }
             else
-            Console.WriteLine("The current temperature is within its normal range [30 - 90C].");
-
+            { 
+                Console.WriteLine("The current temperature is within its normal range [30 - 90C].");
+            }
+            
             int diffTemp = inputCurrentTemp - previouslyRecordedTemp;
             Console.WriteLine("The temperature fluctuation is {0}", diffTemp);
 
             int diffTempAbs = Math.Abs(inputCurrentTemp - previouslyRecordedTemp);
 
             if (diffTempAbs > 20)
+            { 
                 Console.WriteLine("ALARM! The absolute temperature fluctuation is more than 20C!");
+            }
             else
-                Console.WriteLine("The absolute temperature fluctuation is normal.");                 
-           
+            { 
+                Console.WriteLine("The absolute temperature fluctuation is normal.");
+            }
+
             Console.ReadLine();          
             
         }
